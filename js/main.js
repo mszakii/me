f35502903796a842c4f5416a380e2be1e673f65f
Mohammed-Zaky <learnmeprogramming@gmail.com>
2021-10-03 19:18:57 +0200

    My Portfolio

---
+++ js/main.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", function () {
+  setTimeout(function() {
+    load.style.display = "none";
+  }, 3000);
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 625) {
+    cardup.style = "margin-top: 0; transform: scale(1);";
+  } else {
+    cardup.style = "margin-top: 1000px; transform: scale(.5);";
+  }
+});
+
+window.addEventListener("scroll", function () {
+  // skills section
+  if (window.scrollY >= 800) {
+    html.style.width = "90%";
+    css.style.width = "90%";
+    js.style.width = "40%";
+    c.style.width = "5%";
+    py.style.width = "5%";
+    arduino.style.width = "5%";
+  }
+  // language section
+  if (window.scrollY >= 1900) {
+    ar.style.width = "100%";
+    en.style.width = "65%";
+    de.style.width = "10%";
+  }
+});
